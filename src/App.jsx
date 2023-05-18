c60adf61306b83084d2b2279a5e4b5f1ae333ffb
Sterling Newsom <sterlingnewsom808@gmail.com>
2023-05-18 01:51:09 -0400

    added state to input value

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
+import { useState } from "react";
 import "./styles.css"
 
 export default function App(){
-  return <form className ="new-item-form">
+  const [newItem, setNewItem] = useState("") 
+
+
+  function handleSubmit(e){
+      e.preventDefault() 
+  }
+
+  return(
+    <> 
+    <form className ="new-item-form" onSubmit={ handleSubmit }>
       <div className ="form-row">
         <label htmlFor="item">New Item</label>
-        <input type="text" id="item" />
+        <input value={ newItem } onChange={e=> setNewItem(e.target.value)} type ="text" id = "item"/>
       </div>
 
       <button className="btn">ADD</button>
   </form>
+  <h1 className ="header">Todo List</h1>
+  <ul>
+    <li>
+      <label>
+        <input type ="checkbox" />
+        item 1
+      </label>
+      <button className ="btn btn-danger">Delete</button>
+    </li>
+    <li>
+      <label>
+        <input type ="checkbox" />
+        item 2
+      </label>
+      <button className ="btn btn-danger">Delete</button>
+    </li>
+  </ul>
+  </>
+  )
 }
 
